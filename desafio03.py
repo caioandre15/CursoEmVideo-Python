cd8bfa96eddd61f6be3befb333ca1bd24b5470f4
Caio André <caio_andre15@hotmail.com>
2020-11-04 13:54:56 -0300

    Atualizando desafio03

--- desafio03.py
+++ desafio03.py
@@ -2,5 +2,5 @@ print('====== DEAFIO 03 ======')
 n1 = int(input('Primeiro número: '))
 n2 = int(input('Segundo número: '))
 s = n1+n2
-print('A Soma é {}'.format(s))
+print('A soma entre {} e {} é igual a {}!'.format(n1, n2, s))
 
