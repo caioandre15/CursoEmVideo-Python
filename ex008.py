dabb8490166f05a819aa7ec134a451137047806f
Caio André <caio_andre15@hotmail.com>
2020-11-04 19:16:03 -0300

    Exers 007, 008 e 009

---
+++ ex008.py
@@ -0,0 +1,5 @@
+valor = float(input('Digite um valor em metros: '))
+print('Valor em metros: {:.2f}\n'
+      'Valor em centimetros: {:.2f}\n'
+      'Valor em milímetros: {:.2f}'.format(valor, valor*100, valor*1000))
+
