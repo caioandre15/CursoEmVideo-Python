3bfc34231dada62240d8fcda441a6662234b3fb7
Caio André <caio_andre15@hotmail.com>
2020-11-04 16:23:09 -0300

    Atualização do Exer006

---
+++ ex006.py
@@ -0,0 +1,7 @@
+import math
+
+n = int(input('Digite um número: '))
+print('O número que você digitou é: {}\n'
+      ' O seu dobro é: {}\n'
+      ' O seu triplo é: {}\n'
+      ' A sua raiz quadrada é: {}'.format(n, n*2, n*3, math.isqrt(n)))
